b5ca5faa9021a56fd79724bf3eb6d2204618c8ed
alianggitok <alianggmok@gmail.com>
2014-06-13 13:18:28 +0800

    add new items

---
+++ javascript/load-script/loadScript.js
@@ -0,0 +1,19 @@
+//load JavaScript
+function loadScript(url,callback){
+	var script = document.creatElement("script");
+	script.type = "text/javascript";
+	if (script.readyState){ //IE
+		script.onreadystatechange = function(){
+			if(script.readyState == "loaded"||script.readyState == "complete"){
+				script.readystatechange = null;
+				callback();
+			};
+		};
+	}else{ //Others
+		script.onload = function(){
+			callback();
+		};
+	}
+	script.src = url;
+	document.body.appendChild(script);
+};
