4497cf69a0a52118c2e1137f864994645a280a30
alianggitok <alianggmok@gmail.com>
2014-06-14 16:56:13 +0800

    tidy ui-widget

--- javascript/ui-widget/js/jquery-ui-widget.js
+++ javascript/ui-widget/js/jquery-ui-widget.js
@@ -1,5 +1,68 @@
 var ui={
-	/*sel*/
+	/*tabbox*/
+	tabbox: function(obj,motion){
+		$(obj).each(function() {
+			var obj=$(this);
+			var tabObj=obj.find('.tabs .tab');
+			var contObj=obj.find('.conts .cont');
+			
+			contObj.hide();
+			contObj.first().show();
+			tabObj.first().addClass('current').next().addClass('next');
+			tabObj.on(motion,function(){
+				var n=$(this).index();
+				$(this).siblings().andSelf().removeClass('prev current next');
+				$(this).prev().addClass('prev');
+				$(this).addClass('current');
+				$(this).next().addClass('next');
+				contObj.hide().eq(n).show();
+			});
+			if(motion=='click'){
+				tabObj.click(function(e){
+					e.preventDefault();
+				});
+			};
+		});
+			
+	},
+
+	/*ellipsis*/
+	wordsEllipsis: function(obj,ellipsisSymb,adjustLen){
+		adjustLen=(typeof adjustLen=='undefined'?3:adjustLen);
+		ellipsisSymb=(typeof ellipsisSymb=='undefined'?'<span class="ua-ellipsissymb">…</span>':ellipsisSymb);
+		var n=0;
+		function exec(execObj){
+			if(n>=$(obj).length){return false};
+			execObj=$(obj).eq(n);
+			var objMaxHeight=parseInt(execObj.css('max-height'));
+			var objHeight=execObj.height();
+			var objLineHeight=parseInt(execObj.css('line-height'));
+			var heightFix=objHeight/objLineHeight; //lte ie7
+			var wrapperClassName='ua-maxlen-wrapper';
+			var wrapper='<div class="'+wrapperClassName+'">';
+			if(!execObj.children('.'+wrapperClassName).is('.'+wrapperClassName)){
+				execObj.wrapInner(wrapper);
+			};
+			var wrapperObj=execObj.children('.'+wrapperClassName);
+			wrapperObj.html(wrapperObj.html().replace(ellipsisSymb,'').replace(/</g,'&lt;').replace(/>/g,'&gt;'));
+
+			setTimeout(function(){
+				var len=wrapperObj.html().length;
+				while(wrapperObj.height()-heightFix>objHeight||wrapperObj.height()-heightFix>objMaxHeight){
+					len--;
+					wrapperObj.html(wrapperObj.html().replace(ellipsisSymb,'').substr(0,len));
+					if(wrapperObj.height()-heightFix<=objHeight||wrapperObj.height()-heightFix<=objMaxHeight){
+						wrapperObj.html(wrapperObj.html().replace(ellipsisSymb,'').substr(0,len-adjustLen)+ellipsisSymb);
+					};
+				};
+				execObj.html(wrapperObj.html());
+				exec($(obj).eq(n++));
+			},10);
+		};
+		exec($(obj).eq(n));
+	},
+	
+	/*select*/
 	selectStyleInit: function(obj){
 		$(obj).each(function(){
 			
