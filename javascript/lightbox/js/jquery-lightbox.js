89650d493b7126c00b09c76a49657732ca4d689d
alianggitok <alianggmok@gmail.com>
2014-07-21 18:09:31 +0800

    recode

--- javascript/lightbox/js/jquery-lightbox.js
+++ javascript/lightbox/js/jquery-lightbox.js
@@ -1,200 +1,156 @@
-//========================================================
-//	uaLightBox
-//	Depend on jQuery v1.7.2+
-//	Code by Warren Chen
-//	Create date: 2014-7-9
-//========================================================
-
 ;(function ($) {
 
-	var browserAgent = navigator.userAgent,
-		browser = {
-			isIE6: function () {
-				return (/msie 6/i).test(browserAgent);
-			},
-			isIE7: function () {
-				return (/msie 7/i).test(browserAgent);
-			}
-		};
-
 	$.fn.lightBox = function(options){
-		
 		var defaults={
-				triggerGroup:'',
 				boxClass:'lightbox',
-				boxPicClass:'picholder',
-				boxWrapper:'wrapper',
+				boxWrapperClass:'wrapper',
+				picHolderClass:'picholder',
 				origPicSrcAttr:'href',
-				navPrevHTML:'<a class="btn nav-prev" title="上一张"><i class="ico"></i></a>',
-				navNextHTML:'<a class="btn nav-next" title="下一张"><i class="ico"></i></a>',
-				initWidth:300,
-				initHeight:300,
+				navPrevHTML:'<a class="btn nav nav-prev" title="上一张"><i class="ico"></i></a>',
+				navNextHTML:'<a class="btn nav nav-next" title="下一张"><i class="ico"></i></a>',
+				btnCloseHTML:'<a class="btn btn-close" title="关闭"><i class="ico"></i>关闭</a>',
+				refObj:window,
 				effectDuration:600
 			},
-			opts=$.extend(defaults,options);
+			opts=$.extend(defaults,options),
+			boxHTML=''+
+				'<div class="'+opts.boxClass+'">'+
+				'	<div class="'+opts.boxWrapperClass+'">'+
+				'		<div class="'+opts.picHolderClass+'">'+
+				'			<img class="pic" src="#">'+
+				'		</div>'+
+				'		<div class="info">'+
+				'			<span class="tit"></span>'+
+				'			<span class="page"></span>'+
+				'		</div>'+
+				'	</div>'+
+				'</div>',
+			_box=$(boxHTML),
+			_boxWrapper=_box.find('.'+opts.boxWrapperClass),
+			_picHolder=_box.find('.'+opts.picHolderClass),
+			_img=_picHolder.find('.pic'),
+			_imgProto=_img.get(0),
+			_title=_box.find('.info .tit'),
+			_page=_box.find('.info .page'),
+			_navPrev=$(opts.navPrevHTML),
+			_navNext=$(opts.navNextHTML),
+			_ref=$(opts.refObj),
+			_btnClose=$(opts.btnCloseHTML),
+			_trigger=$(this),
+			triggerSelector=$(this).selector,
+			len=_trigger.length,
+			current=0,
+			origPicSrc=[],
+			title=[],
+			checkImgStatusTimer=null,
+			positionDelay=null;
 
-		return this.each(function(n){
-			var boxHTML=''+
-					'<div class="'+opts.boxClass+'">'+
-					'	<div class="wrapper">'+
-					'		<div class="'+opts.boxPicClass+'">'+
-					'			<img class="pic" src="">'+
-					'		</div>'+
-					'		<div class="nav">'+
-					'			'+opts.navPrevHTML+
-					'			'+opts.navNextHTML+
-					'		</div>'+
-					'		<div class="info">'+
-					'			<span class="tit"></span>'+
-					'			<span class="page"></span>'+
-					'		</div>'+
-					'		<a class="btn btn-close" title="关闭"><i class="ico"></i>关闭</a>'+
-					'	</div>'+
-					'</div>',
-				_box=$(boxHTML),
-				_boxWrapper=_box.find('.'+opts.boxWrapper),
-				_boxPic=_box.find('.'+opts.boxPicClass),
-				_boxImg=_boxPic.find('.pic'),
-				_boxImgProto=_boxImg.get(0),
-				_title=_box.find('.info .tit'),
-				_page=_box.find('.info .page'),
-				_navPrev=_box.find('.nav .nav-prev'),
-				_navNext=_box.find('.nav .nav-next'),
-				isLoaded=function(){
-					return _boxImgProto.complete || _boxImgProto.readyState === 'complete' || _boxImgProto.readyState === 'loaded';
-				};
+		for(var i=0; i<len; i++){
+			origPicSrc[i]=_trigger.eq(i).attr(opts.origPicSrcAttr);
+			title[i]=_trigger.eq(i).attr('title');
+		};
 
-			var _ref=$(window),
-				_trigger=$(this).filter(opts.triggerGroup),
-				len=_trigger.length,
-				current=n,
-				bigSrc=[],
-				title=[];
+		console.log('===>'+$(this).selector+': \n'+triggerSelector+', \n'+title+', \n'+origPicSrc);
 
-			for(var i=0; i<len; i++){
-				bigSrc[i]=_trigger.eq(i).attr(opts.origPicSrcAttr);
-				title[i]=_trigger.eq(i).attr('title');
-			};
-
-			function boxPreLoad(){
-				_boxWrapper.children().not(_boxPic).css('display','none');
-				_boxImg.attr('src','').css('display','none');
-				_box.css({
-					'display':'none',
-					'position': 'absolute'
-				}).appendTo('body');
-			};
-			function boxInit(callback){
-				boxPreLoad();
-				_box.stop(false,true).fadeTo(1,opts.effectDuration,function(){
-					_boxPic.animate({
-						'width': opts.initWidth+'px',
-						'height': opts.initHeight+'px'
-					},opts.effectDuration);
-					_box.animate({
-						'top': (_ref.outerHeight()-_box.outerHeight())/2+'px',
-						'left': (_ref.outerWidth()-_box.outerWidth())/2+'px'
-					},opts.effectDuration);
-				});
-				var check=setInterval(function(){
-					if(!(_boxPic.is(':animated')&&_box.is(':animated'))){
-						typeof(callback)==='undefined'?function(){}:callback();
-						clearInterval(check);
-						check=null;
-					};
-				},20);
-			};
-			function boxFixed(){
-				var refHeight=_ref.outerHeight(),
-					refWidth=_ref.outerWidth(),
-					picOrigWidth=_boxImg.outerWidth(),
-					picOrigHeight=_boxImg.outerHeight(),
-					boxHeight=_box.outerHeight(),
-					boxWidth=_box.outerWidth(),
-					boxTop=(refHeight-boxHeight)/2,
-					boxLeft=(refWidth-boxWidth)/2;
-				_boxPic.css({
-					'width':picOrigWidth+'px',
-					'height':picOrigHeight+'px'
-				});
-				_box.css({
-					'top':boxTop+'px',
-					'left':boxLeft+'px'
-				});
-			};
-			function change(current){
-				_boxImg.attr('src',bigSrc[current]);
-				_title.html(title[current]);
-				_page.html(current+1+'/'+len);
-			};
-			function open(current){
-				boxInit(function(){
-					change(current);
-					_boxWrapper.children().not(_boxPic).show();
-					var checkLoadStatusTimer = setInterval(function(){
-						console.log('isload: '+isLoaded())
-						if(isLoaded()){
-							var boxFixedTimer = setInterval(function(){
-								console.log('boxFixed');
-								boxFixed();
-							},20);
-							_boxImg.stop(false,true).show(opts.effectDuration,function(){
-								setTimeout(function(){
-									clearInterval(boxFixedTimer);
-									boxFixedTimer=null;
-								},opts.effectDuration);
-							});
-							clearInterval(checkLoadStatusTimer);
-							checkLoadStatusTimer=null;
-						};
-					},200);
-				});
-			};
-			function close(callback){
-				_box.stop(false,true).fadeOut(opts.effectDuration,function(){
-					_box.remove();
-					typeof(callback)==='undefined'?function(){}:callback();
-				});
-			};
-
-			_trigger.on('click',function(e){
-				_trigger.removeClass('current');
-				$(this).addClass('current');
-				current=n;
-				console.log(current);
-				close(function(){
-					open(current);
-				});
-				e.preventDefault();
+		function isLoaded(obj){
+			return obj.complete || obj.readyState === 'complete' || obj.readyState === 'loaded';
+		};
+		function init(current){
+			var trigger=_trigger.eq(current);
+			clearInterval(checkImgStatusTimer);
+			clearTimeout(positionDelay);
+			_trigger.removeClass('current');
+			trigger.addClass('current');
+			_btnClose.appendTo(_boxWrapper);
+			_navPrev.appendTo(_boxWrapper);
+			_navNext.appendTo(_boxWrapper);
+			_boxWrapper.children().not(_picHolder).css('display','none');
+			_img.attr('src','').css('display','none');
+			_box.css({
+				'display':'none',
+				'position': 'absolute',
+				'top':trigger.offset().top,
+				'left':trigger.offset().left
+			}).appendTo('body');
+			_picHolder.css({
+				'width':trigger.width()-(_box.outerWidth()-_box.width()),
+				'height':trigger.height()-(_box.outerHeight()-_box.height())
 			});
-			_navPrev.on('click',function(){
-				if(current<=0){
-					return false;
+			_box.stop(false,true).fadeTo(opts.effectDuration,1);
+		};
+		function changePic(current,callback){
+			console.log(origPicSrc[current]);
+			_img.attr('src',origPicSrc[current]);
+			_title.html(title[current]);
+			_page.html(current+1+'/'+len);
+			checkImgStatusTimer=setInterval(function(){
+				console.log('check');
+				if(isLoaded(_imgProto)){
+					callback();
+					clearInterval(checkImgStatusTimer);
+					checkImgStatusTimer=null;
 				};
-				change(current-=1);
+			},100);
+		};
+		function boxResize(){
+			_img.css('display','block');
+			_picHolder.stop(false,true).animate({
+				'width':_img.outerWidth(),
+				'height':_img.outerHeight()
+			},opts.effectDuration);
+		}
+		function boxPosition(){
+			var boxWidth=_box.outerWidth()-_picHolder.outerWidth()+_img.outerWidth(),
+				boxHeight=_box.outerHeight()-_picHolder.outerHeight()+_img.outerHeight();
+			_box.stop(false,true).animate({
+				'left':(_ref.width()-boxWidth)/2,
+				'top':(_ref.height()-boxHeight)/2
+			},opts.effectDuration);
+		};
+		function closeBox(callback){
+			_box.stop(false,true).fadeOut(opts.effectDuration,function(){
+				$(this).remove();
 			});
-			_navNext.on('click',function(){
-				if(current+1>=len){
-					return false;
-				};
-				change(current+=1);
-			});
+			clearInterval(checkImgStatusTimer);
+			clearTimeout(positionDelay);
+			$(window).off('resize.lightbox keydown.lightbox');
+		};
 
-			$(window).off('resize.lightbox keydown.lightbox').on({
+		_trigger.on('click',function(e){
+			e.preventDefault();
+			current=$(this).index(triggerSelector);
+			console.log(current);
+			init(current);
+			changePic(current,function(){
+				positionDelay=setTimeout(function(){
+					_boxWrapper.children().not(_picHolder).css('display','block');
+					boxResize();
+					boxPosition();
+				},opts.effectDuration);
+			});
+			$(window).on({
 				'resize.lightbox':
 				function(){
-					boxFixed();
+					boxPosition();
 				},
 				'keydown.lightbox':
 				function(e){
 					var keycode=e.which||e.keyCode;
 					if(keycode===27){
-						close();
+						closeBox();
 					};
+				}
+			});
+			_btnClose.off('click.lightbox').on({
+				'click.lightbox':
+				function(){
+					closeBox();
 				}
 			});
 
 		});
+
+
 
 	};
 
