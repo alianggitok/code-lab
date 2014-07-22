46a0fd76a1083f4f2d4aa7faa83286eca0285009
alianggitok <alianggmok@gmail.com>
2014-07-22 12:32:19 +0800

    coding

--- javascript/lightbox/js/jquery-lightbox.js
+++ javascript/lightbox/js/jquery-lightbox.js
@@ -1,22 +1,32 @@
+/*========================================================*/
+//	lightbox 1.0
+//	Depend on jQuery v1.7.2+
+//	Code by Warren Chen
+//	Create date: 2014-7-9
+/*========================================================*/
+
 ;(function ($) {
 
 	$.fn.lightBox = function(options){
 		var defaults={
-				boxClass:'lightbox',
-				boxWrapperClass:'wrapper',
-				picHolderClass:'picholder',
+				box:'.lightbox',
+				boxWrapper:'.wrapper',
+				picHolder:'.picholder',
 				origPicSrcAttr:'href',
-				navPrevHTML:'<div class="nav nav-prev"><a class="btn" title="上一张"><i class="ico"></i>上一张</a><div>',
-				navNextHTML:'<div class="nav nav-next"><a class="btn" title="下一张">下一张<i class="ico"></i></a><div>',
+				navPrevHTML:'<div class="nav nav-prev"></div>',
+				navNextHTML:'<div class="nav nav-next"></div>',
+				btnPrevHTML:'<a class="btn" title="上一张"><i class="ico"></i>上一张</a>',
+				btnNextHTML:'<a class="btn" title="下一张">下一张<i class="ico"></i></a>',
 				btnCloseHTML:'<a class="btn btn-close" title="关闭"><i class="ico"></i>关闭</a>',
 				refObj:window,
-				effectDuration:300
+				effectDuration:300,
+				sizeLimit:true,
 			},
 			opts=$.extend(defaults,options),
 			boxHTML=''+
-				'<div class="'+opts.boxClass+'">'+
-				'	<div class="'+opts.boxWrapperClass+'">'+
-				'		<div class="'+opts.picHolderClass+'">'+
+				'<div class="'+opts.box.replace('.','')+'">'+
+				'	<div class="'+opts.boxWrapper.replace('.','')+'">'+
+				'		<div class="'+opts.picHolder.replace('.','')+'">'+
 				'			<img class="pic" src="#">'+
 				'		</div>'+
 				'		<div class="info">'+
@@ -26,22 +36,28 @@
 				'	</div>'+
 				'</div>',
 			_box=$(boxHTML),
-			_boxWrapper=_box.find('.'+opts.boxWrapperClass),
-			_picHolder=_box.find('.'+opts.picHolderClass),
+			_boxWrapper=_box.find(opts.boxWrapper),
+			_picHolder=_box.find(opts.picHolder),
 			_img=_picHolder.find('.pic'),
 			_imgProto=_img.get(0),
 			_title=_box.find('.info .tit'),
 			_page=_box.find('.info .page'),
-			_navPrev=$(opts.navPrevHTML),
-			_navNext=$(opts.navNextHTML),
-			_ref=$(opts.refObj),
+			_prev=$(opts.navPrevHTML),
+			_btnPrev=$(opts.btnPrevHTML),
+			_next=$(opts.navNextHTML),
+			_btnNext=$(opts.btnNextHTML),
 			_btnClose=$(opts.btnCloseHTML),
+			_ref=$(opts.refObj),
 			_trigger=$(this),
 			triggerSelector=$(this).selector,
 			len=_trigger.length,
 			current=0,
 			origPicSrc=[],
 			title=[],
+			picOrigWidth=0,
+			picOrigHeight=0,
+			picWidth=0,
+			picHeight=0,
 			checkImgStatusTimer=null,
 			positionDelay=null;
 
@@ -55,15 +71,18 @@
 		function isLoaded(obj){
 			return obj.complete || obj.readyState === 'complete' || obj.readyState === 'loaded';
 		};
-		function init(current){
-			var trigger=_trigger.eq(current);
+		function init(){
 			clearInterval(checkImgStatusTimer);
 			clearTimeout(positionDelay);
+		};
+		function boxInit(current){
+			var trigger=_trigger.eq(current);
+			init();
 			_trigger.removeClass('current');
 			trigger.addClass('current');
 			_btnClose.appendTo(_boxWrapper);
-			_navPrev.appendTo(_boxWrapper);
-			_navNext.appendTo(_boxWrapper);
+			_prev.appendTo(_boxWrapper).append(_btnPrev);
+			_next.appendTo(_boxWrapper).append(_btnNext);
 			_box.css({
 				'display':'none',
 				'position': 'absolute',
@@ -77,7 +96,8 @@
 				'height':height+'px',
 				'line-height':height+'px',
 				'text-align':'center',
-				'vertical-align':'middle'
+				'vertical-align':'middle',
+				'font-size': '0'
 			});
 			_boxWrapper.children().not(_picHolder).css('display','none');
 			_img.attr('src','').css({
@@ -85,36 +105,43 @@
 				'vertical-align':'middle'
 			});
 		};
-		function boxResize(){
+		function boxResize(picOrigWidth,picOrigHeight){
+			_img.stop(false,true).show(opts.effectDuration);
 			_picHolder.stop(false,true).animate({
-				'width':_img.outerWidth()+'px',
-				'height':_img.outerHeight()+'px',
-				'line-height':_img.outerHeight()+'px'
+				'width':picOrigWidth+'px',
+				'height':picOrigHeight+'px',
+				'line-height':picOrigHeight+'px'
 			},opts.effectDuration);
 		}
-		function boxPosition(){
-			var boxWidth=_box.outerWidth()-_picHolder.outerWidth()+_img.outerWidth(),
-				boxHeight=_box.outerHeight()-_picHolder.outerHeight()+_img.outerHeight();
+		function boxPosition(picOrigWidth,picOrigHeight){
+			var refWidth=_ref.width(),
+				refHeight=_ref.height(),
+				boxWidth=_box.outerWidth()-_picHolder.outerWidth()+picOrigWidth,
+				boxHeight=_box.outerHeight()-_picHolder.outerHeight()+picOrigHeight,
+				boxTop=boxHeight>refHeight?0:(refHeight-boxHeight)/2,
+				boxLeft=boxWidth>refWidth?0:(refWidth-boxWidth)/2;
 			_box.stop(false,true).animate({
-				'left':(_ref.width()-boxWidth)/2,
-				'top':(_ref.height()-boxHeight)/2
+				'left':boxLeft,
+				'top':boxTop
 			},opts.effectDuration);
 		};
 		function changePic(current){
-			console.log(origPicSrc[current]);
-			clearInterval(checkImgStatusTimer);
-			clearTimeout(positionDelay);
-			_img.attr('src',origPicSrc[current]).hide();
+			console.log(current+': '+origPicSrc[current]);
+			init();
+			_img.stop(false,true).fadeOut(opts.effectDuration,function(){
+				_img.attr('src',origPicSrc[current]);
+			});
 			_title.html(title[current]);
 			_page.html(current+1+'/'+len);
 			checkImgStatusTimer=setInterval(function(){
-				console.log('check');
+				console.log('checked');
 				if(isLoaded(_imgProto)){
 					positionDelay=setTimeout(function(){
+						picOrigWidth=_img.outerWidth();
+						picOrigHeight=_img.outerHeight();
 						_boxWrapper.children().not(_picHolder).show();
-						_img.stop(false,true).fadeTo(opts.effectDuration,1);
-						boxResize();
-						boxPosition();
+						boxResize(picOrigWidth,picOrigHeight);
+						boxPosition(picOrigWidth,picOrigHeight);
 					},opts.effectDuration);
 					clearInterval(checkImgStatusTimer);
 					checkImgStatusTimer=null;
@@ -133,47 +160,53 @@
 			};
 			changePic(current+=1);
 		};
-		function closeBox(callback){
-			_box.stop(false,true).fadeOut(opts.effectDuration,function(){
+		function closeBox(boxObj){
+			boxObj.stop(false,true).fadeOut(opts.effectDuration,function(){
 				$(this).remove();
 			});
-			clearInterval(checkImgStatusTimer);
-			clearTimeout(positionDelay);
-			$(window).off('resize.lightbox keydown.lightbox');
+			init();
 		};
 
 		_trigger.on('click',function(e){
 			e.preventDefault();
 			current=$(this).index(triggerSelector);
-			console.log(current);
-			init(current);
+			$(window).off('resize.lightbox keydown.lightbox');
+			boxInit(current);
 			changePic(current);
 			$(window).on({
 				'resize.lightbox':
 				function(){
-					boxPosition();
+					boxResize(picOrigWidth,picOrigHeight);
+					boxPosition(picOrigWidth,picOrigHeight);
 				},
 				'keydown.lightbox':
 				function(e){
-					var keycode=e.which||e.keyCode;
-					if(keycode===27){
-						closeBox();
+					var keyCode=e.which||e.keyCode;
+					//console.log('keydown: '+keyCode);
+					if(keyCode===27){
+						closeBox($(opts.box));
+					}else if(keyCode===37){
+						prev();
+						e.preventDefault();
+					}else if(keyCode===39){
+						next();
+						e.preventDefault();
 					};
 				}
 			});
 			_btnClose.off('click.lightbox').on({
 				'click.lightbox':
 				function(){
-					closeBox();
+					closeBox(_box);
 				}
 			});
-			_navPrev.off('click.lightbox').on({
+			_btnPrev.off('click.lightbox').on({
 				'click.lightbox':
 				function(){
 					prev();
 				}
 			});
-			_navNext.off('click.lightbox').on({
+			_btnNext.off('click.lightbox').on({
 				'click.lightbox':
 				function(){
 					next();
